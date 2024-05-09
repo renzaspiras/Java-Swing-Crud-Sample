6033cef2e62ab1cd3c40bb581bd5395e2dbbc3dc
renzaspiras <aspirasrenz@gmail.com>
2024-05-09 18:46:28 +0800

    data update

--- core/data/Config.java
+++ core/data/Config.java
@@ -4,5 +4,5 @@ public class Config {
   public final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
   public final String db_url = "jdbc:mysql://localhost:3306/ipt";  
   public final String username = "root"; 
-  public final String password = "password";
+  public final String password = "";
 }
